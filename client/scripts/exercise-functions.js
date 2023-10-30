f07f04c9789324b37f6a25150c5d1273b1e84835
uyentruong2003 <uyenkaty.truong@gmail.com>
2023-10-30 09:49:54 -0500

    copy frontend from pa2

--- client/scripts/exercise-functions.js
+++ client/scripts/exercise-functions.js
@@ -18,22 +18,44 @@ const getSavedExercises = () => {
     }
 }
 
+//Function
+
 // Function: Remove an exercise:
-const removeExercise = (id) => {
+const removeExercise = (id, exerciseList) => {
     let exerciseIndex = exerciseList.findIndex((exercise) => exercise.id === id);
     if (exerciseIndex > -1) {
         exerciseList.splice(exerciseIndex,1);
         saveExercises(exerciseList);
+        renderExercises(exerciseList);
     } else {
         console.log("Exercise ID is not found.");
     }
 }
 
+// Function: Selection Sort exercises:
+function sortExercises(exerciseList){
+    for(let i=0; i < exerciseList.length-1; i++){
+        let latestIndex = i;
+        for(let j=i+1; j< exerciseList.length; j++){
+            if (exerciseList[j].dateCompleted > exerciseList[latestIndex].dateCompleted) {
+                latestIndex = j;
+            }
+        }
+        // Swap the current latest with the new latest exercise:
+        if (latestIndex != i){
+            let temp = exerciseList[i];
+            exerciseList[i] = exerciseList[latestIndex];
+            exerciseList[latestIndex] = temp;
+        }
+    }
+};
+
+
 // Function: Add new exercise:
 function addNewExercise(exerciseList) {
     // Select Activity Type:
     let selectedType = "none"; 
-
+    // Update the activity selector:
     document.querySelector('#activityTypeDropdown').addEventListener('change', (e) => {
         selectedType = e.target.value;
     });
@@ -46,146 +68,56 @@ function addNewExercise(exerciseList) {
             activityName: document.getElementById("activityNameInput").value,
             activityType: selectedType,
             distance: document.getElementById("distanceInput").value,
-            // dateCompleted: document.getElementById("dateCompleted").value,
+            dateCompleted: document.getElementById("dateCompleted").value,
             pinned: false,
-            deleted: false
         };
         // Add the new exercise to the array & save to local storage:
         exerciseList.push(exercise);
         saveExercises(exerciseList);
         // Reset form after submitting:
         document.getElementById("add-new-form").reset();
+        // Render exercise:
+        exerciseList= getSavedExercises();
+        renderExercises(exerciseList);
     });
     
 }
 
+// Function: Handle delete button:
+function handleDeleteButton(id, exerciseList) {
+    let deleteButton=document.querySelector(`#deleteButton-${id}`);
+    deleteButton.addEventListener('click',() => {
+        removeExercise(id, exerciseList);
+    });
+}
 
-// Function: create HTML dom for  1 new exercise:
-function generateExerciseDOM (exercise) {
-    let imageURL = "";
-    // pick the illustrated pic for the card:
-    if (exercise.activityType === "Bike") {
-        imageURL= "../client/resources/pilates.jpeg";
-    } else if (exercise.activityType === "Run") {
-        imageURL= "../client/resources/pilates.jpeg";
-    } else if (exercise.activityType === "Hike") {
-        imageURL= "../client/resources/pilates.jpeg";
-    } else if (exercise.activityType === "Outdoor Walk") {
-        imageURL= "../client/resources/pilates.jpeg";
-    } else {
-        imageURL= "../client/resources/pilates.jpeg";
-    }
-    // html to build the exercise's card
-    const cardContent= `
-    <div class="card mb-3" style="width: 700px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src=${imageURL} class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${exercise.activityName}</h5>
-              <div class="card-text">
-                <p>Activity Type: ${exercise.activityType}</p>
-                <p>Distance (in Miles): ${exercise.distance}</p>
-                <p>Date Completed: ${exercise.dateCompleted}</p>
-              </div>
-              <div>
-                <button class="btn btn-primary" id="pinButton-${exercise.id}">Pin</button>
-                <button class="btn btn-danger" id="deleteButton-${exercise.id}">Delete</button>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>`;
-    document.querySelector("#exercise-list-container").innerHTML = cardContent;
-
-    //delete button handling
-    document.querySelector(`#deleteButton-${exercise.id}`).addEventListener('click',(e) => {
-        // remove exercise from local storage
-        removeExercise(exercise.id);
-        // get the updated list after removal
-        exerciseList = getSavedExercises();
-        // re-render th elist:
-        renderExercises(exerciseList);
-    })
+// Function: Generate DOM for each new exercise:
+function generateExerciseDOM(exercise, exerciseList) {
+    // row content HTML:
+    const rowContent = `
+    <td>${exercise.activityName}</td>
+    <td>${exercise.activityType}</td>
+    <td>${exercise.distance}</td>
+    <td>${exercise.dateCompleted}</td>
+    <td>
+        <button class="btn btn-primary" id="pinButton-${exercise.id}">Pin</button>
+        <button class="btn btn-danger" id="deleteButton-${exercise.id}">Delete</button>
+    </td>`
+    // create a new row:
+    const newRow = document.createElement('tr');
+    // add the row content HTML into the new row:
+    newRow.innerHTML= rowContent;
+    // add the new row to the table:
+    document.querySelector('#table-body').appendChild(newRow);
+    handleDeleteButton(exercise.id, exerciseList);
 }
 
 function renderExercises(exerciseList) {
-    const container = document.getElementById("exercise-list-container");
-    container.innerHTML = ""; // Clear the container first
-    exerciseList.forEach(exercise => {
-        generateExerciseDOM(exercise);
+    sortExercises(exerciseList);
+    // clear DOM of the table:
+    document.querySelector('#table-body').innerHTML="";
+    // loop thru the list and add the rows into the table:
+    exerciseList.forEach((exercise) => {
+        generateExerciseDOM(exercise, exerciseList);
     });
 }
-
-// function generateExerciseDOM(exercise) {
-//     let imageURL = "";
-
-//     // pick the illustrated pic for the card:
-//     if (exercise.activityType === "Bike") {
-//         imageURL = "../client/resources/pilates.jpeg";
-//     } else if (exercise.activityType === "Run") {
-//         imageURL = "../client/resources/pilates.jpeg";
-//     } else if (exercise.activityType === "Hike") {
-//         imageURL = "../client/resources/pilates.jpeg";
-//     } else if (exercise.activityType === "Outdoor Walk") {
-//         imageURL = "../client/resources/pilates.jpeg";
-//     } else {
-//         imageURL = "../client/resources/pilates.jpeg";
-//     }
-
-//     // HTML for the exercise card
-//     const cardContent = `
-//     <div class="card mb-3" style="width: 700px;">
-//         <div class="row g-0">
-//           <div class="col-md-4">
-//             <img src=${imageURL} class="img-fluid rounded-start" alt="...">
-//           </div>
-//           <div class="col-md-8">
-//             <div class="card-body">
-//               <h5 class="card-title">${exercise.activityName}</h5>
-//               <div class="card-text">
-//                 <p>Activity Type: ${exercise.activityType}</p>
-//                 <p>Distance (in Miles): ${exercise.distance}</p>
-//                 <p>Date Completed: ${exercise.dateCompleted}</p>
-//               </div>
-//               <div>
-//                 <button class="btn btn-primary" id="pinButton-${exercise.id}">Pin</button>
-//                 <button class="btn btn-danger" id="deleteButton-${exercise.id}">Delete</button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//     </div>`;
-
-//     return cardContent; // Return the card content as a string
-// }
-
-// function renderExercises(exerciseList) {
-//     const container = document.getElementById("exercise-list-container");
-//     let allCardContent = ""; // Accumulate all card content
-
-//     exerciseList.forEach(exercise => {
-//         const cardContent = generateExerciseDOM(exercise);
-//         allCardContent += cardContent; // Append each card's content
-//         handleDeleteButton(exercise);
-//     });
-
-//     container.innerHTML = allCardContent; // Set the container's content once
-    
-// }
-
-// function handleDeleteButton(exercise){
-//     //delete button handling
-//     document.querySelector(`#deleteButton-${exercise.id}`).addEventListener('click',(e) => {
-//         // remove exercise from local storage
-//         removeExercise(exercise.id);
-//         // get the updated list after removal
-//         exerciseList = getSavedExercises();
-//         // re-render th elist:
-//         renderExercises(exerciseList);
-//     })
-// }
-
-//Function: sort a-z:
--- client/scripts/index.js
+++ client/scripts/index.js
@@ -2,8 +2,5 @@ let exerciseList = getSavedExercises();
 console.log(exerciseList);
 addNewExercise(exerciseList);
 renderExercises(exerciseList);
-// exerciseList.forEach(exercise => {
-//     removeExercise(exercise.id);
-// });
 
 
